d56df8a0c073008d3bff0127dd5024fcb76d82fa
Zach Wolek <zach.wolek@gmail.com>
2024-02-06 21:53:03 -0600

    show 7/13 finished

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,8 +1,34 @@
+function createHobbit(hobbitName, hobbitAge){
+  var hobbit = {
+    name: hobbitName,
+    age: hobbitAge,
+    isAdult: false,
+    isOld: false
+  }
+  if (hobbitAge === undefined){
+    hobbit.age = 0
+  if (hobbitName === undefined){
+    hobbit.name = 'unknown'
+  }
+  };
+console.log(hobbit)
+  return hobbit
+};
+
+function celebrateBirthday(hobbitChar){
+  hobbitChar.age += 1;
+  if (hobbitChar.age >= 33){
+    hobbitChar.isAdult = true}
+  if (hobbitChar.age >= 101){
+    hobbitChar.isOld = true
+  }
+  return hobbitChar
+};
 
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
+ createHobbit, 
+  celebrateBirthday, 
   // getRing, 
   // meetPeople, 
   // findFriends
