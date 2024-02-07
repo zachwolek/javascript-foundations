4d12b3535e155de4f82a0252ae19edd2901a3610
Zach Wolek <zach.wolek@gmail.com>
2024-02-06 21:20:00 -0600

    Show the end of dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,42 @@
+function createDragon(dragonName, dragonRider, dragonTemp){
+  var dragon = {
+      name: dragonName,
+      rider: dragonRider,
+      temperment: dragonTemp,
+      timesEaten: 0,
+      hungry: true, 
+};
+return dragon
+}
+
+function greetRider(dragonName){
+return `Hi, ${dragonName["rider"]}!`
+}
+
+function eat(dragon){
+ dragon["timesEaten"] += 1;
+ if (dragon.timesEaten >= 3)
+ {dragon.hungry = false}; 
+ return dragon
+  }
+
+  function findFireBreathers(allDragons){
+    fireBreathers = []
+    for (var i = 0; i < allDragons.length; i++){
+      var dragon = allDragons[i];
+      if(dragon.temperment === 'aggressive'){
+        fireBreathers.push(dragon)
+      }
+    }
+    return fireBreathers
+  }
+
+  createDragon('Lady Vox', 'Emily', 'gentle')
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
