b7ddc02d0f8869d8bee45b7cf3948e09c922f177
Zach Wolek <zach.wolek@gmail.com>
2024-02-10 22:59:12 -0600

    show the end of the vampire exercises

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -13,22 +13,42 @@ function createVampire(vampName, vampPet){
 
 function encounterDeliciousVictim(vampire){ if (vampire.thirsty === true){
   return 'I WANT TO SUCK YOUR BLOOD!'
-}
+} else 
+return `No thanks, I am too full.`
 };
 
 function drink(vampName){
-  vampName.ouncesDrank += 10
+  if (vampName.ouncesDrank <=40){
+    vampName.ouncesDrank += 10}
   if (vampName.ouncesDrank >= 50){
-    vampName.ouncesDrank = 50;
     vampName.thirsty = false;
   };
   return vampName
+};
+
+function inquirePlace(locations, expectedLocation){
+ if (locations.includes(expectedLocation)){
+    return `Yes, I have spent some time in ${expectedLocation}.`
+ } else
+ return `No, I have never been to ${expectedLocation}.`
+};
+
+function findBatLovers(vampire){
+  batLovers = []
+  for (i = 0; i < vampire.length; i++){
+    if (vampire[i].pet === 'bat'){
+
+        batLovers.push(vampire[i].name)
+    }
+}
+console.log(batLovers)
+return batLovers
 }
 
 module.exports = {
 createVampire, 
 encounterDeliciousVictim, 
 drink, 
-// findBatLovers, 
-  // inquirePlace
+inquirePlace,
+findBatLovers, 
 }
