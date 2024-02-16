84ff546dcca33332173c6f436b575e9377c208fe
Zach Wolek <zach.wolek@gmail.com>
2024-02-16 10:52:40 -0600

    Finish Birthdays

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,30 @@
+function createBirthday(name, month, day) {
+    var birthday = {
+        name,
+        month,
+        day
+    }
+    return birthday
+}
+
+function celebrateBirthday(birthday) {
+    return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`
+}
 
 
-module.exports = {  };
+function countBirthdays(birthday, month){
+    var count = 0
+    for (var i = 0; i < birthday.length; i++)
+    if(birthday[i].month === month){
+        count += 1
+    }
+    return count
+}
+var count = 0
+
+
+module.exports = { 
+    createBirthday, 
+    celebrateBirthday, 
+    countBirthdays  
+};
